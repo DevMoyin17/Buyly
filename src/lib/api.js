4ca85f4507958b08ea7e25320aa5337ce6a1910f
Moyinoluwa <moyinoluwaadigun390@gmail.com>
2025-09-08 13:01:30 -0700

    backend url update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:3000/api';
-const API_URL = 'https://buyly-backend.onrender.com';
+// const API_URL = 'https://buyly-backend.onrender.com';
+const API_URL = 'https://buyly-backend.vercel.app/';
 
 
 export const api = {
