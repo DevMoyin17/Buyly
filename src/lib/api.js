2a04450cdb58d11b36e6c10d404ba09ad5b45d0b
Moyinoluwa <moyinoluwaadigun390@gmail.com>
2025-09-08 12:45:19 -0700

    new update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:3000/api';
-const API_URL = 'https://buyly-backend-1.onrender.com';
+const API_URL = 'https://buyly-backend.onrender.com';
 
 
 export const api = {
